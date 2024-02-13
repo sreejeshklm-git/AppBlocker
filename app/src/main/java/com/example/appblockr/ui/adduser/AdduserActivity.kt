91697c1ed538afdaf0f9b83e9e7b596cf7185e4f
Sekh Miraj1 <sm00913503@techmahindra.com>
2024-02-13 16:17:15 +0530

    add user dashboard

--- app/src/main/java/com/example/appblockr/ui/adduser/AdduserActivity.kt
+++ app/src/main/java/com/example/appblockr/ui/adduser/AdduserActivity.kt
@@ -1,8 +1,13 @@
 package com.example.appblockr.ui.adduser
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import androidx.appcompat.app.ActionBar
 import androidx.databinding.DataBindingUtil
+import com.example.appblockr.MainActivity
 import com.example.appblockr.R
 import com.example.appblockr.databinding.ActivityAdduserBinding
 import com.example.appblockr.firestore.FireStoreManager
@@ -14,6 +19,9 @@ class AdduserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM;
+        supportActionBar?.setCustomView(R.layout.title_bar);
+        supportActionBar?.elevation = 0F
         binding = DataBindingUtil.setContentView(this, R.layout.activity_adduser)
 
         fireStoreManager = FireStoreManager()
@@ -22,21 +30,23 @@ class AdduserActivity : AppCompatActivity() {
 
 
       binding.btnAddUser.setOnClickListener{
-
-          val user = User(
-              email = binding.edtEmail.text.toString(),
-              password = binding.edtPassword.text.toString(),
-              user_name = binding.edtUserName.text.toString(),
-              1
-          )
-          val data = hashMapOf(
-              "email" to "${user.email}",
-              "password" to "${user.password}",
-              "user_name" to "${user.user_name}",
-              "user_type" to "${user.user_type}",
-          )
-          fireStoreManager.addDataToFireStoreDB(data,"add_users")
-          clearEditFields()
+          if (checkAllFields()) {
+              val user = User(
+                  email = binding.edtEmail.text.toString(),
+                  password = binding.edtPassword.text.toString(),
+                  user_name = binding.edtUserName.text.toString(),
+                  1
+              )
+              val data = hashMapOf(
+                  "email" to "${user.email}",
+                  "password" to "${user.password}",
+                  "user_name" to "${user.user_name}",
+                  "user_type" to "${user.user_type}",
+              )
+              fireStoreManager.addDataToFireStoreDB(data, "add_users")
+              clearEditFields()
+             // startActivity(Intent(this, MainActivity::class.java))
+          }
       }
 
     }
@@ -45,5 +55,27 @@ class AdduserActivity : AppCompatActivity() {
         binding.edtEmail.text?.clear()
         binding.edtPassword.text?.clear()
         binding.edtUserName.text?.clear()
+    }
+
+    private fun checkAllFields(): Boolean {
+        if (binding.edtUserName.length() == 0) {
+            binding.edtUserName.error = "User Name is required"
+            return false
+        }
+        if (binding.edtEmail.length() == 0 || !isValidEmail(binding.edtEmail.text.toString())) {
+            binding.edtEmail.error = "Email is required"
+            return false
+        }
+        if (binding.edtPassword.length() == 0 || !isValidPassword(binding.edtPassword.text.toString())) {
+            binding.edtPassword.error = "Password is required & minimum six digits"
+            return false
+        }
+        return true
+    }
+    private fun isValidEmail(email: String): Boolean {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+    private fun isValidPassword(password: String): Boolean {
+        return password.length >= 6
     }
 }
